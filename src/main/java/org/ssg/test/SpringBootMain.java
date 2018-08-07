dbfbaeb484b70f1fe9cbaa46b46357cbfc5ad267
shenshiguan <1097000406@qq.com>
2018-08-07 15:18:50 +0800

    //2.0 更改嵌入式容器端口  然并卵

--- src/main/java/org/ssg/test/SpringBootMain.java
+++ src/main/java/org/ssg/test/SpringBootMain.java
@@ -2,13 +2,24 @@ package org.ssg.test;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
 @SpringBootApplication
-public class SpringBootMain extends SpringBootServletInitializer {
+public class SpringBootMain extends SpringBootServletInitializer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(SpringBootMain.class);
 	}
+
+	@Override
+	public void customize(ConfigurableWebServerFactory factory) {
+		//2.0 更改嵌入式容器端口  然并卵
+		factory.setPort(8081);
+	}
+	
+	
 }
