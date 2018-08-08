84e64fccb1f89001d4f8d19365c3a955d8e1c487
shenshiguan <1097000406@qq.com>
2018-08-08 11:14:45 +0800

    return html

--- src/main/java/org/ssg/test/action/Test1Controller.java
+++ src/main/java/org/ssg/test/action/Test1Controller.java
@@ -6,16 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Test1Controller {
 
-	@RequestMapping("/test1/index")
+	@RequestMapping("/test1/index1")
 	public String index1(){
 		System.out.println("..............index1.............");
-		return "Hello World!";
+		return "Hello World index1!";
 	}
 	
-	@RequestMapping("/test2/index")
+	@RequestMapping("/test1/index2")
 	public String index2(){
 		System.out.println("..............index2.............");
-		return "Hello World!";
+		return "Hello World index2!";
 	}
 	
 	
